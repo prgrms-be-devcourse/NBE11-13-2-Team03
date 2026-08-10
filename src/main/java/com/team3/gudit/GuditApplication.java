98a94edaa41423b760239e6917e08bd40610af01
Ascody <actaney@gmail.com>
2026-08-10 09:51:10 +0900

    first commit

---
+++ src/main/java/com/team3/gudit/GuditApplication.java
@@ -0,0 +1,13 @@
+package com.team3.gudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuditApplication.class, args);
+    }
+
+}
