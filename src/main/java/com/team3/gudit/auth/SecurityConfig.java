8637615adf5d65e90f5d4e4c58ab74e08685d8bc
Ascody <actaney@gmail.com>
2026-08-11 14:05:27 +0900

    Fix: config에 CustomOAuth2UserService 연결 추가

--- src/main/java/com/team3/gudit/auth/SecurityConfig.java
+++ src/main/java/com/team3/gudit/auth/SecurityConfig.java
@@ -1,13 +1,16 @@
 package com.team3.gudit.auth;
 
+import com.team3.gudit.auth.filter.TokenAuthenticationFilter;
 import com.team3.gudit.auth.oauth2.OAuth2FailureHandler;
 import com.team3.gudit.auth.oauth2.OAuth2SuccessHandler;
+import com.team3.gudit.auth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +19,10 @@ public class SecurityConfig {
 
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final OAuth2FailureHandler oAuth2FailureHandler;
+    private final TokenAuthenticationFilter tokenAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomOAuth2UserService customOAuth2UserService) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
@@ -26,14 +30,26 @@ public class SecurityConfig {
                                 "/login-success.html",
                                 "/login-failure.html",
                                 "/oauth2/**",
-                                "/login/**"
+                                "/login/**",
+                                "/api/auth/reissue"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
 
+                // 소셜 로그인
                 .oauth2Login(oauth -> oauth
+                        .userInfoEndpoint(
+                                userInfo ->
+                                        userInfo.userService(customOAuth2UserService)
+                        )
                         .successHandler(oAuth2SuccessHandler)
                         .failureHandler(oAuth2FailureHandler)
+                )
+
+                // access 토큰 검사
+                .addFilterBefore(
+                        tokenAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
         return http.build();
     }
