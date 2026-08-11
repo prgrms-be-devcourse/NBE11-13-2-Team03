e6c7b19d86b1876a1bac4bb249abd4516be2a89b
Ascody <actaney@gmail.com>
2026-08-11 14:07:29 +0900

    Feat: access token / refresh token 생성시 구분자 추가

--- src/main/java/com/team3/gudit/auth/service/TokenService.java
+++ src/main/java/com/team3/gudit/auth/service/TokenService.java
@@ -4,10 +4,12 @@ import com.team3.gudit.auth.dto.RefreshTokenResponseDto;
 import com.team3.gudit.auth.jwt.JwtProperties;
 import com.team3.gudit.auth.jwt.TokenProvider;
 import com.team3.gudit.auth.jwt.TokenStatus;
+import com.team3.gudit.auth.jwt.TokenType;
 import com.team3.gudit.user.domain.entity.User;
 import com.team3.gudit.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,12 +28,14 @@ public class TokenService {
 
         String accessToken = tokenProvider.generateToken(
                 user,
-                jwtProperties.getAccessTokenValidity()
+                jwtProperties.getAccessTokenValidity(),
+                TokenType.ACCESS
         );
 
         String refreshToken = tokenProvider.generateToken(
                 user,
-                jwtProperties.getRefreshTokenValidity()
+                jwtProperties.getRefreshTokenValidity(),
+                TokenType.REFRESH
         );
 
         return new TokenPair(
@@ -40,19 +44,26 @@ public class TokenService {
         );
     }
 
+    @Transactional
     public TokenPair refreshToken(String refreshToken) {
-
         if (refreshToken == null) {
             throw new IllegalArgumentException(
                     "Refresh Token이 없습니다."
             );
         }
 
-        if (tokenProvider.validateToken(refreshToken)
-                != TokenStatus.VALID) {
-
+        if (tokenProvider.validateToken(
+                refreshToken,
+                TokenType.REFRESH
+        ) != TokenStatus.VALID) {
             throw new IllegalArgumentException(
-                    "유효하지 않은 Refresh Token입니다."
+                    "Refresh Token이 없거나 유효하지 않습니다."
+            );
+        }
+
+        if (tokenProvider.getTokenType(refreshToken) != TokenType.REFRESH) {
+            throw new IllegalArgumentException(
+                    "Refresh Token이 아닙니다."
             );
         }
 
